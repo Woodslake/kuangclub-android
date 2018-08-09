77abaada6de4d223d3f3f29fe6e838335fba9995
Woodslake <woodslakelee@gmail.com>
2018-08-09 19:17:43 +0800

    ...

--- app/src/main/java/com/kuangclub/ui/fragment/QuotationFragment.java
+++ app/src/main/java/com/kuangclub/ui/fragment/QuotationFragment.java
@@ -3,28 +3,17 @@ package com.kuangclub.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kuangclub.R;
-import com.kuangclub.ui.adapter.HomeRecyclerAdapter;
 import com.kuangclub.ui.base.BaseFragment;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Woodslake on 2018/7/27.
  */
 public class QuotationFragment extends BaseFragment {
-    private RecyclerView recyclerView;
-
-    private HomeRecyclerAdapter homeRecyclerAdapter;
-
-    private List<String> list;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,33 +36,11 @@ public class QuotationFragment extends BaseFragment {
     @Override
     protected void initData(@Nullable Bundle savedInstanceState) {
         super.initData(savedInstanceState);
-        list = new ArrayList<>();
-        homeRecyclerAdapter = new HomeRecyclerAdapter(list);
     }
 
     @Override
     protected void initView(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.initView(view, savedInstanceState);
-        recyclerView = view.findViewById(R.id.recycler_view);
-
-        recyclerView = view.findViewById(R.id.recycler_view);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(homeRecyclerAdapter);
-
-        refresh();
-    }
-
-    private void refresh(){
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < 50; i++){
-            String str = "i = " + i;
-            list.add(str);
-        }
-        this.list.clear();
-        this.list.addAll(list);
-        homeRecyclerAdapter.notifyDataSetChanged();
     }
 
 }
