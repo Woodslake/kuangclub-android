bb521c058820e07f3839c1ddc8a0b97d77060b48
Woodslake <woodslakelee@gmail.com>
2018-08-10 23:18:09 +0800

    ...

--- app/src/main/java/com/kuangclub/socket/WsManager.java
+++ app/src/main/java/com/kuangclub/socket/WsManager.java
@@ -1,7 +1,57 @@
 package com.kuangclub.socket;
 
+import android.support.annotation.Nullable;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
+
 /**
  * Created by Woodslake on 2018/8/10.
  */
 public class WsManager {
+    private static String TAG = "WsManager";
+
+    public static void connect(final String wsUrl, final WebSocketListener webSocketListener){
+        Request request = new Request.Builder()
+                .url(wsUrl)
+                .build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.newWebSocket(request, new WebSocketListener() {
+            @Override
+            public void onOpen(WebSocket webSocket, Response response) {
+                super.onOpen(webSocket, response);
+                webSocketListener.onOpen(webSocket, response);
+            }
+
+            @Override
+            public void onMessage(WebSocket webSocket, String text) {
+                super.onMessage(webSocket, text);
+            }
+
+            @Override
+            public void onMessage(WebSocket webSocket, ByteString bytes) {
+                super.onMessage(webSocket, bytes);
+            }
+
+            @Override
+            public void onClosing(WebSocket webSocket, int code, String reason) {
+                super.onClosing(webSocket, code, reason);
+            }
+
+            @Override
+            public void onClosed(WebSocket webSocket, int code, String reason) {
+                super.onClosed(webSocket, code, reason);
+            }
+
+            @Override
+            public void onFailure(WebSocket webSocket, Throwable t, @Nullable Response response) {
+                super.onFailure(webSocket, t, response);
+            }
+        });
+    }
+
 }
