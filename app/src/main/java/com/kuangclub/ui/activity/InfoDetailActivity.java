f6d34b1a8902a41bbd280be5c8a26a150f60dd66
Woodslake <woodslakelee@gmail.com>
2018-08-10 22:31:14 +0800

    ...

--- app/src/main/java/com/kuangclub/ui/activity/InfoDetailActivity.java
+++ app/src/main/java/com/kuangclub/ui/activity/InfoDetailActivity.java
@@ -1,25 +1,46 @@
 package com.kuangclub.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
 
 import com.kuangclub.R;
+import com.kuangclub.ui.base.BaseActivity;
 
-public class InfoDetailActivity extends AppCompatActivity {
+public class InfoDetailActivity extends BaseActivity {
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_detail);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        initData(savedInstanceState);
+        initView(savedInstanceState);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    @Override
+    protected void initData(@Nullable Bundle savedInstanceState) {
+        super.initData(savedInstanceState);
+    }
+
+    @Override
+    protected void initView(@Nullable Bundle savedInstanceState) {
+        super.initView(savedInstanceState);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/kuangclub/ui/base/BaseActivity.java
+++ app/src/main/java/com/kuangclub/ui/base/BaseActivity.java
@@ -1,9 +1,25 @@
 package com.kuangclub.ui.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Woodslake on 2018/7/27.
  */
 public class BaseActivity extends AppCompatActivity {
+    protected String TAG = getClass().getSimpleName();
+
+    protected void initData(@Nullable Bundle savedInstanceState){
+
+    }
+
+    protected void initView(@Nullable Bundle savedInstanceState){
+
+    }
+
+    protected void refreshData(){
+
+    }
+
 }
