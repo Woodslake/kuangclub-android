3d58def1f8db3c206d8fab4e42a7338f8cfa375b
Woodslake <woodslakelee@gmail.com>
2018-08-10 22:13:23 +0800

    ...

--- app/src/main/java/com/kuangclub/ui/activity/InfoDetailActivity.java
+++ app/src/main/java/com/kuangclub/ui/activity/InfoDetailActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 import com.kuangclub.R;
@@ -28,4 +29,9 @@ public class InfoDetailActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
 }
